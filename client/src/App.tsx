6553d1c159fb93182c98a990fe502b3642ddfcf2
denismezhenin <103366013+denismezhenin@users.noreply.github.com>
2023-07-02 17:31:12 +0200

    feat: add Error boundary

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,22 @@ import { MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './utils/router';
+import { ErrorBoundary } from 'react-error-boundary';
+import { StrictMode } from 'react';
+import { ErrorFallback } from './components/ErrorFallback';
 
 function App() {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Notifications position="bottom-right" zIndex={10} />
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </MantineProvider>
+    <StrictMode>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+          <Notifications position="bottom-right" zIndex={10} />
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </MantineProvider>
+      </ErrorBoundary>
+    </StrictMode>
   );
 }
 
