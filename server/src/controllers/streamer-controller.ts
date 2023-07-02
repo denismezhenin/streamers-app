e4d53603ea2ce75b3a4b29857794696b8d27ee42
denismezhenin <103366013+denismezhenin@users.noreply.github.com>
2023-07-02 21:12:49 +0200

    fix: delete redundants

--- server/src/controllers/streamer-controller.ts
+++ server/src/controllers/streamer-controller.ts
@@ -1,4 +1,3 @@
-import { error } from "console";
 import { Request, Response } from "express";
 import { Streamer } from "../models/streamer";
 
@@ -45,7 +44,6 @@ const getStreamerByID = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
     const streamer = await Streamer.findById(id);
-    console.log(streamer)
     res.status(200).json(streamer);
   } catch (err) {
     handleError(res, err);
@@ -53,16 +51,9 @@ const getStreamerByID = async (req: Request, res: Response) => {
 };
 
 const updateStreamerVotes = async (req: Request, res: Response) => {
-  // console.log(1)
   try {
     const { id, upVotesNumber, downVotesNumber } = req.body;
-    // console.log(req.body)
     const streamer = await Streamer.findById(id);
-    // if (streamer?.upVotes) {
-    //  console.log(1)
-    //   return
-    // }
-    console.log(`up: ${upVotesNumber}, down:${downVotesNumber}`);
     const updateStreamersVotes = await Streamer.findByIdAndUpdate(
       id,
       {
@@ -71,11 +62,7 @@ const updateStreamerVotes = async (req: Request, res: Response) => {
       },
       { new: true }
     );
-    // console.log(streamer)
-    console.log(updateStreamersVotes)
-    // setTimeout(() => {
       res.status(200).json(updateStreamersVotes);
-    // }, 2000);
   } catch (err) {
     handleError(res, err);
   }
